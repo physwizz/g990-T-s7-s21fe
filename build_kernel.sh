7f8297133b59663ac0e170556bde06685bf8d0f0
physwizz <pollockg86@gmail.com>
2023-11-14 13:56:05 +1100

    v1 setup

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+make clean && make mrproper
 export ARCH=arm64
 export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
 export ANDROID_MAJOR_VERSION=t
 
 make ARCH=arm64 physwizz_defconfig
 make ARCH=arm64 -j16
+
